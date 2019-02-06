b28b6e80b3f7ef3cdb75dbd290a2d6081020bb12
Nipinium <nipinium@gmail.com>
2019-02-06 19:57:05 +0700

    Bump version to 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pygments.Mixfile do
   def project do
     [
       app: :pygments,
-      version: "0.1.1",
+      version: "1.0.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env == :prod,
       deps: deps()
